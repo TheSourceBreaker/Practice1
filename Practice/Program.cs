7ebd9895dc7ae56191ca2fbe071b0bd90cdae3f6
Jahvier Hernandez <jahvierhernandez11@gmail.com>
2019-08-27 17:26:41 -0700

    YellowBook

---
+++ Practice/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static private int width_of_window = 3;
+        static private int height_of_window = 6;
+        
+
+        static private int Wood()
+        {
+            int length = (width_of_window + height_of_window) *2;
+            int result = Wood();
+            return result;
+        }
+        static private int Glass()
+        {
+            int area = width_of_window * height_of_window;
+            int result = Glass();
+            return result;
+        }
+        
+
+
+
+        static void Main(string[] args)
+        {
+            Wood();
+            Glass();
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
